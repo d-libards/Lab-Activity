54fd90c7d985fadf8b77fe9430738b584f1e21b8
blancojames88 <blancojames88@gmail.com>
2023-06-08 00:59:51 +0800

    added react and tailwind config for la3-finals

---
+++ LA3-FINALS/la3-finals/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-3xl font-bold text-center mt-8">
+        Welcome to My React App with Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
